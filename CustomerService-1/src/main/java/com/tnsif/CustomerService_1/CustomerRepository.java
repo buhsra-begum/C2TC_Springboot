f433e71435987e1002a4c5c86e98d30684e590b7
bushy-ash <bushyansari30@gmail.com>
2025-11-07 22:16:42 +0530

    commit

---
+++ CustomerService-1/src/main/java/com/tnsif/CustomerService_1/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.tnsif.CustomerService_1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+}
+	
---
+++ CustomerService-1/src/main/java/com/tnsif/CustomerService_1/CustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.CustomerService_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+}
